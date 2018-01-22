5122d18b22a0e81e6fa6d09271747201bbc98194
A.H. Khodabakhsh <a.h.khodabakhsh@gmail.com>
2018-01-22 15:12:28 +0330

    minor

--- Methods/NFSOnLoad.m
+++ Methods/NFSOnLoad.m
@@ -1,4 +1,4 @@
 %% Run this script once you want to change the model structure
 
-TurbulenceSelector = 0; % 0 - no turbulence; 1 - Dryden; 2 - Von-Karman
-NavigationSelector = 0;
+TurbulenceSelector = 1; % 0 - no turbulence; 1 - Dryden; 2 - Von-Karman
+NavigationSelector = 3;
